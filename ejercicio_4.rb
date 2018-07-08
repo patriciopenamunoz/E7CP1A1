f644a91be362f58c6466957afef8a60f3205082a
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-07-08 18:18:16 -0400

    Añadir método 'obtener_promedio'

--- ejercicio_4.rb
+++ ejercicio_4.rb
@@ -1,7 +1,14 @@
+def obtener_promedio(hash)
+  hash.values.inject(0) { |sum, n| sum + n }.to_f / hash.length
+end
+
 personas = ['Carolina', 'Alejandro', 'Maria Jesús', 'Valentín']
 edades = [32, 28, 41, 19]
 
 hash = {}
 personas.each_with_index { |value, index| hash[value] = edades[index] }
 
-print hash
+# Crear un metodo que reciba el hash y devuelva el promedio de las edades
+# del hash pasado como argumento.
+
+print obtener_promedio(hash)
