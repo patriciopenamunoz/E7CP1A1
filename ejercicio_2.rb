52c9fa0a16e7963c4ecebcfe495aebb302d5f23f
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-07-08 17:40:37 -0400

    Completar tareas restantes.

--- ejercicio_2.rb
+++ ejercicio_2.rb
@@ -1,2 +1,20 @@
-productos = {'bebida' => 850, 'chocolate' => 1200, 'galletas' => 900, 'leche' => 750}
-productos.each { |producto,valor| puts producto }
+productos = {
+  bebida: 850,
+  chocolate: 1200,
+  galletas: 900,
+  leche: 750
+}
+
+# Se quiere agregar un nuevo producto al hash.
+productos[:cereal] = 2200
+
+# Se quiere actualizar el precio de la bebida.
+productos[:bebida] = 2000
+
+# Convertir el hash en un array y guardarlo en una nueva variable.
+arr_productos = productos.to_a
+
+# Eliminar el producto 'galletas' del hash.
+productos.delete(:galletas)
+
+productos.each { |producto, valor| puts producto }
